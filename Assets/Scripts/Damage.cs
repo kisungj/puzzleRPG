db6922b3dfd83b0bf6a5a60e581d73ff58dc5f19
kisung-cloud <naxkyt12@gmail.com>
2020-09-28 01:26:36 +0900

    UI 구조 수정 완료

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Damage : MonoBehaviour
+{
+    public float hp = 300.0f;
+    private float maxHp = 300.0f;
+    private float guage = 0;
+    public float maxGuage = 100.0f;
+
+    [SerializeField]
+    private Text hpText;
+
+    public Slider hpSlider;
+    public Slider guageSlider;
+
+    void Update()
+    {
+        hpSlider.value = hp / maxHp;
+        guageSlider.value = guage / maxGuage;
+        hpText.text = hp + " / " + maxHp;
+
+        hitDamage();
+    }
+
+    //공격 당했을 때
+    public void hitDamage()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            hp -= 10;
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyHp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHp : MonoBehaviour
+{
+    public float hp = 0;
+    private float maxHp = 500.0f;
+    private GameObject fhpBar;
+    private GameObject fhpText;
+
+    [SerializeField]
+    private Text hpText;
+    public Slider hpSlider; 
+
+    void Start()
+    {
+        hpSlider.value = hp / maxHp;
+        hpText.text = hp + " / " + maxHp;
+
+        //해당 오브젝트 이름 찾아준다.
+        fhpBar = GameObject.Find("UICanvas/EnemyUI/HpBar");
+        fhpText = GameObject.Find("UICanvas/EnemyUI/HpText");
+    }
+
+    void Update()
+    {
+        //오브젝트에 따른 위치 이동 (월드좌표를 화면좌표로 변환시켜준다)
+        fhpBar.transform.position = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, -0.2f, 0));
+        fhpText.transform.position = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, -0.2f, 0));
+    }
+}
